f1be99f6b7d023da21c06ce55881f3d5c710d0b6
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-08-18 23:43:10 +0900

    setup and template

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <img src="/neko.png" alt="" class="neko-img" />
+    </div>
+    <div>{{ scripts[scriptIndex] }}</div>
+    <button @click="proceedScript">NEXT</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+const scripts = ['hello', 'world', 'goodnight']
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      scripts,
+      scriptIndex: 0,
+    }
+  },
+  methods: {
+    proceedScript() {
+      this.scriptIndex += 1
+      if (scripts.length <= this.scriptIndex) {
+        window.alert('finish')
+      }
+    },
+  },
+})
+</script>
+
+<style>
+.neko-img {
+  width: 300px;
+  height: 300px;
+}
+</style>
