7eaef13c1251c52d0d958a8bc99d9c2075154a20
knknk98 <65712721+knknk98@users.noreply.github.com>
2022-08-19 21:00:55 +0900

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,4 +48,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  router: {
+    base: '/tsukutte-asobu2022/',
+  },
+
+  generate: {
+    dir: 'docs',
+  },
 }
