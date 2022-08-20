210cba4ad0678462b50ddaa8e89a67bf4fcaed63
knknk98 <65712721+knknk98@users.noreply.github.com>
2022-08-20 16:19:21 +0900

    スタート画面のスタイル調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <div v-if="!start" class="start-page">
-      <img src="/startgamen.png" />
-      <button class="start-button" @click="start = true">はじめる</button>
+      <img src="/kawaiineko.png" />
+      <div class="right-side">
+        <div class="description">可愛らしい猫で日頃の疲れを癒しませんか？</div>
+        <button class="start-button" @click="start = true">はじめる</button>
+        <p class="attention">画像はイメージです</p>
+      </div>
     </div>
     <Nuxt v-else />
   </div>
@@ -30,24 +34,54 @@ export default {
   position: relative;
   width: 100vw;
   height: 100vh;
+  background: url('../static/start-haikei.JPG');
+  background-size: cover;
+  display: flex;
+  padding: 30px;
+  align-items: center;
+}
+
+img {
+  width: 50%;
+  height: 40vw;
+}
+
+.description {
+  font-family: serif;
+  font-size: 200%;
+  margin-bottom: 10%;
+  color: #6f4b3e;
+}
+
+.right-side {
+  width: 50%;
+  height: 40vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
 .start-button {
-  position: fixed;
-  right: 5%;
-  bottom: 5%;
   background-color: palevioletred;
+  font-size: 200%;
   border: none;
   cursor: pointer;
   outline: 3px solid white;
   padding: 10px 30px;
   appearance: none;
-  font-size: 48px;
   border-radius: 30px;
   color: white;
 }
 
 .start-button:hover {
   background-color: pink;
+}
+
+.attention {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  color: gray;
+  font-size: 30%;
 }
 </style>
