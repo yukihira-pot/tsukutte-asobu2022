eba342b819b11e794ebe7bc385d970991bc5bb99
knknk98 <65712721+knknk98@users.noreply.github.com>
2022-08-20 14:49:39 +0900

    クイズ機能の実装

---
+++ components/Quiz.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="quiz-container">
+    <div class="quiz">ここに問題を書きます</div>
+    <textarea v-model="yourAnswer" class="answer" />
+    <button @click="SendAnswer">回答</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Quiz',
+  data() {
+    return {
+      yourAnswer: '',
+    }
+  },
+  methods: {
+    SendAnswer() {
+      const QuestionAnswer = '答え'
+      if (this.yourAnswer === QuestionAnswer) {
+        window.alert('正解や')
+        this.$emit('change-mode', 'endAngry')
+      } else {
+        window.alert('ちゃうわ！')
+      }
+      this.yourAnswer = ''
+    },
+  },
+})
+</script>
+
+<style scoped>
+.quiz-container {
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  z-index: 300;
+  width: 100%;
+  height: 75%;
+  padding-top: 3rem;
+  align-items: flex-end;
+}
+
+.quiz {
+  background: white;
+  margin-right: 3%;
+  width: 40%;
+  height: 100%;
+  padding: 1rem;
+  border: 0.1vw solid black;
+  border-radius: 0.8vw;
+}
+
+.answer {
+  margin-right: 3%;
+  width: 40%;
+  height: 100%;
+  padding: 1rem;
+  border: 0.1vw solid black;
+  border-radius: 0.8vw;
+}
+
+button {
+  position: absolute;
+  top: 80%;
+  left: 80%;
+  width: 8vw;
+  height: 8vw;
+  border-radius: 50%;
+  font-size: 2vw;
+  cursor: pointer;
+}
+
+button:hover {
+  background: yellow;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,10 @@
       :mode="mode"
       @change-mode="changeMode"
     />
+    <Quiz
+      v-if="mode == 'doAngry' && angryMode == 'quiz'"
+      @change-mode="changeMode"
+    />
     <NekoPageMain :mode="mode" />
     <div v-if="angryMode == ''" class="feed-btn-container">
       <button class="feed-btn" @click="mode = 'feed'">
@@ -27,14 +31,6 @@
     {{ mode }}
     {{ angryMode }}
   </div>
-  <!-- <div>
-        <div>{{ scripts[scriptIndex] }}</div>
-        <button @click="proceedScript">NEXT</button>
-        <button @click="submitHanseibun">提出</button>
-        <textarea v-model="yourAnswer" />
-        <button @click="GiveQuestion">問題</button>
-        <button @click="SendAnswer">解答</button>
-      </div> -->
 </template>
 
 <script>
@@ -43,6 +39,7 @@ import NekoPageMain from '../components/NekoPageMain.vue'
 import NekoPageSpeech from '../components/NekoPageSpeech.vue'
 import Hanseibun from '../components/Hanseibun.vue'
 import BlueScreen from '@/components/BlueScreen.vue'
+import Quiz from '@/components/Quiz'
 const scripts = ['hello', 'world', 'goodnight']
 
 export default Vue.extend({
@@ -52,12 +49,12 @@ export default Vue.extend({
     NekoPageSpeech,
     Hanseibun,
     BlueScreen,
+    Quiz,
   },
   data() {
     return {
       scripts,
       scriptIndex: 0,
-      yourAnswer: '',
       mode: 'normal',
       time: 0,
       timer: null,
@@ -66,7 +63,7 @@ export default Vue.extend({
   },
   watch: {
     time() {
-      if (this.time === 10) {
+      if (this.time === 3) {
         this.mode = 'startAngry'
       }
     },
@@ -77,18 +74,6 @@ export default Vue.extend({
     }, 1000)
   },
   methods: {
-    GiveQuestion() {
-      window.alert('問題出題')
-    },
-    SendAnswer() {
-      const QuestionAnswer = '答え'
-      if (this.yourAnswer === QuestionAnswer) {
-        window.alert('正解!')
-      } else {
-        window.alert('不正解')
-      }
-      this.yourAnswer = ''
-    },
     changeAngryMode(m) {
       this.angryMode = m
     },
