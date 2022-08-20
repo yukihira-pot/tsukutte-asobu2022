c8fcbcd4a284c123034f9bac5902b420dd3f8810
knknk98 <65712721+knknk98@users.noreply.github.com>
2022-08-20 15:50:48 +0900

    スタート画面の実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <Nuxt />
+    <div v-if="!start" class="start-page">
+      <img src="/startgamen.png" />
+      <button class="start-button" @click="start = true">はじめる</button>
+    </div>
+    <Nuxt v-else />
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      start: false,
+    }
+  },
   head: {
     link: [
       {
@@ -15,3 +24,30 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.start-page {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.start-button {
+  position: fixed;
+  right: 5%;
+  bottom: 5%;
+  background-color: palevioletred;
+  border: none;
+  cursor: pointer;
+  outline: 3px solid white;
+  padding: 10px 30px;
+  appearance: none;
+  font-size: 48px;
+  border-radius: 30px;
+  color: white;
+}
+
+.start-button:hover {
+  background-color: pink;
+}
+</style>
